056fcc73ada6e1acf2e94bdf98d1427bf9fdb599
darkhunterbg <kaloyan.r.nachev@gmail.com>
2017-09-28 15:09:44 +0300

    Added bindings generator (only win32/CLI)

---
+++ src/BindingsGenerator/Program.cs
@@ -0,0 +1,73 @@
+﻿using CppSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingsGenerator
+{
+    class Program
+    {
+        const String ExtraHeaderFileName = "CppSharp.h";
+        const String GenFileName = "gen";
+
+#if WIN32
+        const String LibPlatform = "Win32";
+#endif
+
+        static void Main(string[] args)
+        {
+            var currentDir = Directory.GetCurrentDirectory();
+            var outputDir = Path.Combine(currentDir, "bindings");
+
+            String sourceDir = Path.GetFullPath(Path.Combine(currentDir, $@"..\..\..\src"));
+            String libDir = currentDir;
+            XenoCoreLibrary library = new XenoCoreLibrary(LibPlatform)
+            {
+                SourceDir = sourceDir,
+                LibDir = libDir
+            };
+
+            String genFile = Path.Combine(outputDir, GenFileName);
+
+            if (File.Exists(genFile))
+            {
+                var genModified = File.GetLastWriteTime(genFile);
+
+                var libModified = File.GetLastWriteTime(Path.Combine(currentDir, library.LibraryName));
+
+                if (libModified < genModified)
+                {
+                    Console.WriteLine("Library was not modified. Skipping binding generation...");
+                    return;
+                }
+            }
+
+            GenerateBindings(currentDir, outputDir, library);
+
+            File.WriteAllText(genFile, DateTime.Now.ToString());
+        }
+
+        static void GenerateBindings(String currentDir, String outputDir, ILibrary library)
+        {
+
+
+            if (Directory.Exists(outputDir))
+                Directory.Delete(outputDir, true);
+
+            Directory.CreateDirectory(outputDir);
+
+            Directory.SetCurrentDirectory(outputDir);
+
+            //Generate code
+            ConsoleDriver.Run(library);
+
+            File.Copy(Path.Combine(currentDir, ExtraHeaderFileName), Path.Combine(outputDir, ExtraHeaderFileName));
+
+
+            Directory.SetCurrentDirectory(currentDir);
+        }
+    }
+}
---
+++ src/BindingsGenerator/XenoCoreLibrary.cs
@@ -0,0 +1,67 @@
+﻿using CppSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CppSharp.AST;
+using CppSharp.Generators;
+using System.IO;
+
+namespace BindingsGenerator
+{
+    class XenoCoreLibrary : ILibrary
+    {
+        const String ProjectName = "XenoCore";
+
+        public readonly String PlatformName ;
+        public readonly String LibraryName;
+        public readonly String LibraryFileName;
+
+        public String SourceDir { get; set; }
+        public String LibDir { get; set; }
+
+
+        public XenoCoreLibrary( String platformName)
+        {
+            this.PlatformName = platformName;
+            LibraryName = $"{ProjectName}.{PlatformName}";
+
+            LibraryFileName = $"{LibraryName}.lib";
+        }
+
+        public void Postprocess(Driver driver, ASTContext ctx)
+        {
+        }
+
+        public void Preprocess(Driver driver, ASTContext ctx)
+        {
+        }
+
+        public void Setup(Driver driver)
+        {
+            var options = driver.Options;
+            options.GeneratorKind = GeneratorKind.CLI;
+            var module = options.AddModule(LibraryName);
+
+            module.IncludeDirs.Add(Path.Combine(SourceDir, ProjectName));
+            module.IncludeDirs.Add(Path.Combine(SourceDir, module.LibraryName));
+
+            foreach (var includeDir in module.IncludeDirs)
+            {
+
+                foreach (var file in Directory.GetFiles(includeDir, "*.h"))
+                {
+                    module.Headers.Add(file.Substring(includeDir.Length + 1));
+                }
+            }
+            module.LibraryDirs.Add(LibDir);
+            module.Libraries.Add($"{module.LibraryName}.lib");
+
+        }
+
+        public void SetupPasses(Driver driver)
+        {
+        }
+    }
+}
